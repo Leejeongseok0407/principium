0351c9228c54e35b5ac00e0dd46dfe76e939e8a0
이정석 <jsuk970407@gmail.com>
2020-07-03 16:35:52 +0900

    점프 리팩토링
    jumpMaxCount를 넣어주어 최대 점프 횟수를 넣어주고 jumpcount를 조절하는 것으로 점프를 관리하게 만들었다.
    transform.Translate으로 지속적으로 변경 해주는것이 아닌
    addforce를 이용해서 GetButtonDown시 (누를시) 한번 점프하게 만듬
    Ontregger을 이용해서 점프 카운트를 초기화 해주는 방법을 생각했으나 잘 되지 않았음. 나중에 수정 요망

--- Assets/Player/Script/playerBehaviour.cs
+++ Assets/Player/Script/playerBehaviour.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class playerBehaviour : MonoBehaviour
 {
     [SerializeField] int playerHp;
@@ -9,65 +10,107 @@ public class playerBehaviour : MonoBehaviour
     [SerializeField] int jumpPower = 10;
     [SerializeField] int dashPower = 20;
     [SerializeField] int SkillTime = 1;
+    [SerializeField] int jumpMaxCount = 2;
+    int jumpCount;
+    Rigidbody2D rigidbody;
     Vector2 gravity2D;
     bool gravityOn = true;
+    bool isGrounded = false;
+    float keyHorizontal;
+    float keyVertical;
 
     // Start is called before the first frame update
     void Start()
     {
+        jumpCount = jumpMaxCount;
         gravity2D = Physics2D.gravity;
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        keyHorizontal = Input.GetAxis("Horizontal");
+        keyVertical = Input.GetAxis("Vertical");
+    }
+    void FixedUpdate()
+    {
         Move();
-        if (Input.GetKeyDown(KeyCode.Z))
+
+        if (Input.GetButtonDown("SkillKey"))
         {
             PlayerSkill(1);
         }
     }
 
-    void PlayerMove() { 
-    
-    }
 
     //플레이어 스킬을 1~7까지 만드는게 좋을듯.
     //아니면 플레이어 스킬을 2~3가지 넣어서 한번에 2~3가지 실행
     //이건 기획을봐야함.
-    void PlayerSkill(int SkillNum) {
+    void PlayerSkill(int SkillNum)
+    {
         //대쉬 스킬
-        if (SkillNum == 0){
-            float keyHorizontal = Input.GetAxis("Horizontal");
+        if (SkillNum == 0)
+        {
             transform.Translate(Vector3.right * dashPower * keyHorizontal, Space.World);
         }
         //부유 스킬
-        if (SkillNum == 1) {
+        if (SkillNum == 1)
+        {
             Physics2D.gravity = Vector2.zero;
             gravityOn = false;
             Invoke("returnGravity", SkillTime);
         }
-    
+
 
     }
 
-    void returnGravity(){
+    void returnGravity()
+    {
         Physics2D.gravity = gravity2D;
         gravityOn = true;
-
     }
 
     void Move()
     {
-        float keyHorizontal = Input.GetAxis("Horizontal");
-        float keyVertical = Input.GetAxis("Vertical");
+        //좌우 이동
         transform.Translate(Vector3.right * speed * Time.smoothDeltaTime * keyHorizontal, Space.World);
-        if (gravityOn){
-            if(keyVertical > 0)
-            transform.Translate(Vector3.up * jumpPower * Time.smoothDeltaTime * keyVertical, Space.World);
+
+        //상하 이동
+        //gravity가 켜져있으면 점프만
+        if (gravityOn)
+        {
+            //키메니저에 있는 input에서 Jump input실행
+            if (Input.GetButtonDown("Jump"))
+            {
+                Debug.Log(jumpCount);
+                if (jumpCount > 0)
+                {
+                    rigidbody.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+                    jumpCount--;
+                }
+            }
         }
         else
+        {
             transform.Translate(Vector3.up * jumpPower * Time.smoothDeltaTime * keyVertical, Space.World);
 
+        }
+
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("트리거");
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            Debug.Log("닿음");
+        }
+        if (collision.gameObject.tag == "Ground")
+        {
+            Debug.Log("땅에에 닿음");
+            isGrounded = true;    //Ground에 닿으면 isGround는 true
+            jumpCount = 2;          //Ground에 닿으면 점프횟수가 2로 초기화됨
+        }
     }
 }
