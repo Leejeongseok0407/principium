c1fd74bb95c115d95debc31ee08a4c2bba2f9a82
이정석 <jsuk10@naver.com>
2020-07-05 21:08:16 +0900

    태그,레이어 구성, 몬스터 스크립트 만듬, 슬라임 스크립트 만듬
    Player,Monster 태그및 레이어를 구성하였음
    몬스터 스크립트를 만들어 플에이어와 출돌 체크를 하게함
    몬스터 스크립틀르 상속하는 슬라임 스크립트 생성

--- Assets/Monster/Script/monsterBehaviour.cs
+++ Assets/Monster/Script/monsterBehaviour.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//abstract 추상클래스임을 명시
 public class monsterHaviour : MonoBehaviour
 {
     [SerializeField] int type;
     [SerializeField] int hp;
+    [SerializeField] int dmg;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,18 +20,29 @@ public class monsterHaviour : MonoBehaviour
     }
 
     // virtual로 오버라이딩 가능하게 만들어줌.
+    // 이동 모션을 의미함
     protected virtual void Patten(int type)
     {
 
     }
 
+    //굳이 있어야 하나 싶은데..
     protected virtual void Dead()
     {
 
     }
 
+    //
     protected virtual void Attack()
     {
+        
+    }
 
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            print("플레이어와 부딛힘");
+        }
     }
 }
