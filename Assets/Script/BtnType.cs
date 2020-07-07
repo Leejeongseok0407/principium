5d52184255c17106267648159d2c35e8a7165cf4
HyejinMa <mahj345@gmail.com>
2020-07-07 23:38:56 +0900

    버튼 이름 수정
    목적에 맞게 버튼 이름 수정함
    BtnType, MainUI에 enum 변수와 관련된 실행문 제거

--- Assets/Script/BtnType.cs
+++ Assets/Script/BtnType.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class BtnType : MonoBehaviour
 {
-    public BTNType currentType;
     public void OnBtnClick() // 버튼 UI의 OnClick 이벤트에 연결
     {
-        switch (currentType)
+        switch (string type)
         {
-            case BTNType.New:
+            case start:
                 Debug.Log("처음부터");
                 break;
-            case BTNType.Continue:
+            case load:
                 Debug.Log("불러오기");
                 break;
         }
--- Assets/Script/MainUI.cs
+++ Assets/Script/MainUI.cs
@@ -2,16 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum BTNType
-{
-    New,
-    Continue,
-    Option,
-    Sound,
-    Back,
-    Quit
-
-}
 
 public class MainUI : MonoBehaviour
 {
