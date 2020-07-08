a02424c63305eafa8dd001c35bc3e996f3705429
이정석 <jsuk10@naver.com>
2020-07-08 19:51:02 +0900

    SIlme namespace변경
    몬스터 스크립트 변경후 안에 코드 수정안해줫기때문에
    MonsterHaviour로 수정해줌.

--- Assets/Script/Slime.cs
+++ Assets/Script/Slime.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Slime : monsterHaviour
+public class Slime : MonsterHaviour
 {
     // Start is called before the first frame update
     void Start()
