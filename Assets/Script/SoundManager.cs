42beeceddac30db0b5043837fbeb9675cb6f62a3
HyejinMa <mahj345@gmail.com>
2020-07-13 20:11:14 +0900

    pull 오류 해결 위해 커밋함
    그저께부터 이런 현상이 있는데 뭘 수정했는지 가늠이 안 감

--- Assets/Script/SoundManager.cs
+++ Assets/Script/SoundManager.cs
@@ -4,23 +4,24 @@ using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
 
-/*
-public AudioMixer masterMixer;
-public Slider audioSlider;
-
 
 public class SoundManager : MonoBehaviour
 {
+    public AudioMixer masterMixer;
+    public Slider audioSlider;
 
     public void AudioControl()
     {
         float sound = audioSlider.value;
 
-        if (sound == -40f) masterMixer.SetFloat("BGM", -80); // -40일때 -80을 설정한 이유는 음소거 효과를 내기 위함
-        else masterMixer.SetFloat("BGM", sound);
+        if (sound == -40f) 
+            masterMixer.SetFloat("BGM", -80); // -40일때 -80을 설정한 이유는 음소거 효과를 내기 위함
+
+        else 
+            masterMixer.SetFloat("BGM", sound);
     }
 
  
     
 }
-*/
+
