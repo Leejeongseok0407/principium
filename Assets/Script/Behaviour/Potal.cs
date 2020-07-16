f7919ec6c85c64e28b10d3ec3787105a084f0a84
이정석 <jsuk10@naver.com>
2020-07-16 19:33:58 +0900

    포탈 스크립트수정
    포탈 에 온트리거랑 엑시트 트리거를 수정해서
    테그가 플레이어 인 트리거가 들어오면 화살표를 엑티브 하고
    나가면 언엑티브 한다.

--- Assets/Script/Behaviour/Potal.cs
+++ Assets/Script/Behaviour/Potal.cs
@@ -6,11 +6,25 @@ using UnityEngine.SceneManagement;
 public class Potal : MonoBehaviour
 {
     [SerializeField] string scenename = "";
+    [SerializeField] GameObject pressKey;
+
+    private void Start()
+    {
+        pressKey.SetActive(false);
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Player") {
-            SceneManager.LoadScene(scenename);
+        if (collision.gameObject.tag == "Player")
+        {
+            pressKey.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+                SceneManager.LoadScene(scenename);
         }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+            pressKey.SetActive(false);
     }
 }
