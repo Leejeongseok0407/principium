8ea2682e6b3c37e7421b12c98ae9c595f205f78a
이정석 <jsuk10@naver.com>
2020-07-16 20:18:29 +0900

    포탈 수정
     플레이어가 안에 있을때 위 방향키를 누르면 신을 이동하게 설정 하였음.

--- Assets/Script/Behaviour/Potal.cs
+++ Assets/Script/Behaviour/Potal.cs
@@ -18,9 +18,14 @@ public class Potal : MonoBehaviour
         if (collision.gameObject.tag == "Player")
         {
             pressKey.SetActive(true);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
             if (Input.GetKeyDown(KeyCode.UpArrow))
                 SceneManager.LoadScene(scenename);
-        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
