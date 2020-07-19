5c862a03bfa7a5310a395571dc635d448485d828
HyejinMa <mahj345@gmail.com>
2020-07-19 23:49:03 +0900

    stage1에 Canvas 생성
    example 씬에서 OptionMenu 오브젝트 복제해왔음
    OptionTrigger 스크립트를 생성하고 옵션창을 열때 게임 실행을 멈추도록 하는 코드 작성
    게임을 실행해보니 Option 버튼이 제대로 작동하지 않음 추후 수정 필

---
+++ Assets/Script/UI/OptionTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OptionTrigger : MonoBehaviour
+{
+    public GameObject optionCanvas;
+
+    public void OptionDown()
+    {
+        Time.timeScale = 0;
+        optionCanvas.SetActive(true);
+    }
+}
