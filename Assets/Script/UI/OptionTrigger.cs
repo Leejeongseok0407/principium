d365cd274f774b92ce11119a957ead3cdf14b4bc
HyejinMa <mahj345@gmail.com>
2020-07-21 19:08:14 +0900

    옵션에서 메인화면 불러오는 버튼 기능 구현
    OptionTrigger 스크립트에서 LoadStartScene 함수에 씬을 전환하는 실행문을 작성

--- Assets/Script/UI/OptionTrigger.cs
+++ Assets/Script/UI/OptionTrigger.cs
@@ -7,9 +7,40 @@ public class OptionTrigger : MonoBehaviour
 {
     public GameObject OptionCanvas;
 
-    public void OptionDown()
+    public void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            Time.timeScale = 0;
+            OptionCanvas.SetActive(true);
+
+            if (Input.GetButtonDown("Cancel")) // ESC 한번 더 누르면 옵션 창 나가기
+            {
+                OptionCanvas.SetActive(false);
+                Time.timeScale = 1;
+            }
+                
+        }
+            
+    }
+    public void OptionDown() // 옵션 버튼 누르면 옵션 창 활성화
     {
         Time.timeScale = 0;
         OptionCanvas.SetActive(true);
     }
+    public void LoadingStartScene() // 옵션창에서 시작화면으로 가는 버튼
+    {
+        SceneManager.LoadScene("start Scene");
+    }
+
+    public void StartGame() // 시작화면에서 게임 처음부터 시작하는 버튼
+    {
+        SceneManager.LoadScene("stage1");
+    }
+    public void ExitGame() // 시작화면에서 게임 종료하기
+    {
+        Application.Quit();
+    }
+
 }
+
