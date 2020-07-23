2d384fab0ec72d859b539b61723117c50998bb6f
HyejinMa <mahj345@gmail.com>
2020-07-23 14:14:15 +0900

    옵션 세부 버튼 클릭시 해당 내용 열기
    OptionTrigger의 각 변수에 대상 적용해서 해결

--- Assets/Script/UI/OptionTrigger.cs
+++ Assets/Script/UI/OptionTrigger.cs
@@ -6,15 +6,18 @@ using UnityEngine.SceneManagement;
 public class OptionTrigger : MonoBehaviour
 {
     public GameObject OptionCanvas;
+    public GameObject SoundCanvas;
+    public GameObject ShortcutCanvas;
+    public GameObject TeamCanvas;
 
     public void Update()
     {
-        if (Input.GetButtonDown("Cancel"))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             Time.timeScale = 0;
             OptionCanvas.SetActive(true);
 
-            if (Input.GetButtonDown("Cancel")) // ESC 한번 더 누르면 옵션 창 나가기
+            if (Input.GetKeyDown(KeyCode.Escape)) // ESC 한번 더 누르면 옵션 창 나가기
             {
                 OptionCanvas.SetActive(false);
                 Time.timeScale = 1;
@@ -28,6 +31,28 @@ public class OptionTrigger : MonoBehaviour
         Time.timeScale = 0;
         OptionCanvas.SetActive(true);
     }
+
+    public void OpenSound() // 사운드 버튼 누르면 사운드 창 활성화
+    {
+        ShortcutCanvas.SetActive(false);
+        TeamCanvas.SetActive(false);
+        SoundCanvas.SetActive(true);
+    }
+
+    public void OpenShortCut() // 단축키 설명 버튼 누르면 단축키 버튼 활성화
+    {
+        SoundCanvas.SetActive(false);
+        TeamCanvas.SetActive(false);
+        ShortcutCanvas.SetActive(true);
+    }
+
+    public void OpenTeam()
+    {
+        SoundCanvas.SetActive(false);
+        ShortcutCanvas.SetActive(false);
+        TeamCanvas.SetActive(true);
+    }
+
     public void LoadingStartScene() // 옵션창에서 시작화면으로 가는 버튼
     {
         SceneManager.LoadScene("start Scene");
