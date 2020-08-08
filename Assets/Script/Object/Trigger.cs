45ac1663dd0a5730df63c07500e1110384a5f133
이정석 <jsuk10@naver.com>
2020-08-08 23:04:27 +0900

    플레이어가 들어갔을때 방향키 위키를 누르면 작동하는 Trigger제작
    OnTriggerEnter/Exit2D를 이용하여 만들었음
    Stay를 이용해서 플레이어가 안에 들어갔을때만 입력 받을 수 있게 제작 하려 했으나 플레이어에게 Collider이 2개 있어서 두번 호출되기 떄문에 불가능하였으며
    이를 쿨타임을 이용해서 해결하려 했으나 이것또한 Stay가 플레이어의 움직임이 있을떄 잠깐만 반응하여 불가능하였음.

---
+++ Assets/Script/Object/Trigger.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    [SerializeField] GameObject pressSprite;
+    [SerializeField] GameObject wall;
+    bool coolTime;
+    bool inPlayer = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pressKeyActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(inPlayer); if (inPlayer == true && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Set");
+            WallKeyActive(!wall.activeSelf);
+        }
+    }
+
+    void pressKeyActive(bool set)
+    {
+        if (pressSprite != null)
+            pressSprite.SetActive(set);
+    }
+
+    void WallKeyActive(bool set)
+    {
+        if (coolTime == true)
+            return;
+        if (wall != null)
+        {
+            wall.SetActive(set);
+            StartCoroutine("CoolTime");
+        }
+    }
+
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            pressKeyActive(true);
+            inPlayer = true;
+        }
+    }
+
+   /* private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("in");
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("PlayerIn");
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                Debug.Log("Set");
+                WallKeyActive(!wall.activeSelf);
+            }
+        }
+    }*/
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            pressKeyActive(false);
+            inPlayer = false;
+        }
+    }
+
+    IEnumerator CoolTime()
+    {
+        coolTime = true;
+        yield return new WaitForSeconds(1f);
+        coolTime = false;
+    }
+}
