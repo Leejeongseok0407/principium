525c1792aced17207af5bbbac4b3ecec4acd21d2
HyejinMa <mahj345@gmail.com>
2020-08-10 00:58:40 +0900

    플레이어의 체력을 표시하는 UI 기능 구현
    switch 문으로 플레이어의 체력이 1씩 닳으면 하트 이미지가 하나씩 사라지도록 스크립트 작성함

---
+++ Assets/Script/UI/Hearts.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections; 
+using System.Collections.Generic; 
+using UnityEngine; 
+using UnityEngine.UI; 
+
+ public class Hearts : MonoBehaviour
+{ 
+
+     public static int hp = 5; 
+
+     public GameObject life1; 
+     public GameObject life2; 
+     public GameObject life3;
+     public GameObject life4;
+     public GameObject life5;
+
+   
+
+     // Use this for initialization 
+
+     void Start()
+    {
+         life1.GetComponent<Image>().enabled = true;
+         life2.GetComponent<Image>().enabled = true;
+         life3.GetComponent<Image>().enabled = true;
+         life4.GetComponent<Image>().enabled = true;
+         life5.GetComponent<Image>().enabled = true;
+    } 
+
+   
+
+     // Update is called once per frame 
+
+     void Update()
+    {
+        
+        switch(hp)
+
+        {
+            case 4:
+                life5.GetComponent<Image>().enabled = false;
+                break;
+
+            case 3:
+                life4.GetComponent<Image>().enabled = false;
+                break;
+
+            case 2: 
+                 life3.GetComponent<Image>().enabled = false;
+                 break;
+        
+             case 1: 
+                 life2.GetComponent<Image>().enabled = false;
+                 break;
+        
+             case 0: 
+                 life1.GetComponent<Image>().enabled = false;
+                 //game over 
+                 break;
+        
+         } 
+
+     } 
+
+ } 
+
