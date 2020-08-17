a8309141cbb98508465930cb5a71532b29f7da49
HyejinMa <mahj345@gmail.com>
2020-08-18 00:42:59 +0900

    메인화면에 적용되는 스크립트 복구

---
+++ Assets/Script/UI/StartBTNTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartBTNTrigger : MonoBehaviour
+{
+    public GameObject MainMenu;
+
+    public void StartGame() // 시작화면에서 게임 처음부터 시작하는 버튼
+    {
+        SceneManager.LoadScene("stage1");
+    }
+    public void ExitGame() // 시작화면에서 게임 종료하기
+    {
+        Application.Quit();
+    }
+}
