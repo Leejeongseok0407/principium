c7be8a1ef49bc971f182b546b504a59eeec3b660
HyejinMa <mahj345@gmail.com>
2020-08-25 01:36:31 +0900

    FadeIn 클래스로 페이드인 구현
    게임오버시 페이드인 되는 효과 코드 작성

---
+++ Assets/Script/UI/FadeIn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeIn : MonoBehaviour
+{
+	IEnumerator CoFadeIn(float fadeOutTime, System.Action nextEvent = null)
+	{
+		SpriteRenderer sr = this.gameObject.GetComponent<SpriteRenderer>();
+		Color tempColor = sr.color;
+		while (tempColor.a < 1f)
+		{
+			tempColor.a += Time.deltaTime / fadeOutTime;
+			sr.color = tempColor;
+
+			if (tempColor.a >= 1f) tempColor.a = 1f;
+
+			yield return null;
+		}
+
+		sr.color = tempColor;
+		if (nextEvent != null) nextEvent();
+	}
+}
+
+
--- Assets/Script/UI/Hearts.cs
+++ Assets/Script/UI/Hearts.cs
@@ -28,5 +28,7 @@ using UnityEngine.UI;
         }
     }
 
+   
+
 } 
 
