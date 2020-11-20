ec9740b4aafa1813db48fbb68fe62c4781ea4f51
이정석 <jsuk10@naver.com>
2020-11-20 19:41:21 +0900

    style : update Thorns

---
+++ Assets/Script/Object/OnOffGround.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnOffGround : MonoBehaviour
+{
+    [SerializeField] float intervalTime = 2f;
+    [SerializeField] float StartTime = 1f;
+    [SerializeField] GameObject ground;
+
+    float time;
+
+
+    private void Update()
+    {
+        time += Time.deltaTime;
+        if (time > StartTime + intervalTime)
+        GroundOnAndOff();
+    }
+
+    void GroundOnAndOff() {
+        if (ground.activeSelf == false)
+            ground.SetActive(true);
+        else 
+            ground.SetActive(false);
+        time = StartTime;
+    }
+
+}
---
+++ Assets/Script/Object/TurnoffGround.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnoffGround : MonoBehaviour
+{
+    [SerializeField] float intervalTime = 2f;
+    [SerializeField] GameObject ground;
+    float time;
+
+
+    private void Update()
+    {
+        if (ground.activeSelf == false)
+        {
+            time += Time.deltaTime;
+            if (time > intervalTime)
+                GroundOn();
+        }
+    }
+
+    void GroundOn()
+    {
+        ground.SetActive(true);
+        time = 0;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("aa");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("dd");  
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            Debug.Log("dd");
+            StartCoroutine(OffGround());
+        }
+    }
+
+    IEnumerator OffGround()
+    {
+        yield return new WaitForSeconds(intervalTime);
+        ground.SetActive(false);
+    }
+}
