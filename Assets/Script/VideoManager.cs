40fe7dfd9965c3aa4c08797767129a946efdc404
HyejinMa <mahj345@gmail.com>
2020-11-26 18:54:54 +0900

    stage1 마지막 씬 영상 삽입
    stage1 마지막 포탈에 video 씬 로드하도록 하여 video 씬이 끝나면 메인화면으로 넘어가도록 함

---
+++ Assets/Script/VideoManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VideoManager : MonoBehaviour
+{
+    public float time = 0;    
+
+    public void Update()
+    {
+        time += Time.deltaTime;
+        if (time > 18)
+            SceneManager.LoadScene("start Scene");
+
+    }
+
+}
