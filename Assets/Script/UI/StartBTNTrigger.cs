78417cecfe581536e22032af484ce716ef01dc56
HyejinMa <mahj345@gmail.com>
2020-11-30 12:12:54 +0900

    메인화면에서 실행되는 옵션 버튼 중 '메인화면' 가기 삭제
    메인화면에서 옵션창을 누를 시 메인메뉴 버튼이 그대로 보이는 버그 발견
    Update에 if문으로 옵션창이 활성화되면 메인화면은 비활성화 되는 코드 작성하였으나
    최초 실행시에만 잘 작동됨

--- Assets/Script/UI/StartBTNTrigger.cs
+++ Assets/Script/UI/StartBTNTrigger.cs
@@ -6,7 +6,15 @@ using UnityEngine.SceneManagement;
 public class StartBTNTrigger : MonoBehaviour
 {
     public GameObject MainMenu;
+    public GameObject OptionDetail;
 
+    void Update()
+    {
+        if (OptionDetail.activeSelf == false) //Option 창 활성화 안 돼있으면 메인메뉴 창 활성화
+            MainMenu.SetActive(true);
+        else if(OptionDetail.activeSelf == true)
+            MainMenu.SetActive(false);
+    }
     public void StartGame() // 시작화면에서 게임 처음부터 시작하는 버튼
     {
         SceneManager.LoadScene("prologue");
@@ -14,5 +22,6 @@ public class StartBTNTrigger : MonoBehaviour
     public void ExitGame() // 시작화면에서 게임 종료하기
     {
         Application.Quit();
+        Debug.Log("끄읏");
     }
 }
